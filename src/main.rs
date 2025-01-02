34a6bd9d62831cfb317595de318b7e0fea6d510b
devsheva <sheshi.mateo01@gmail.com>
2025-01-02 20:06:34 +0100

    chore(add): init add car cmd

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,39 @@
+use clap::{Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(author("Mateo"), version("0.0.1"), about, long_about = None)]
+struct Cli {
+    #[command(subcommand)]
+    command: Option<Commands>,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    #[command(about = "Add a new car")]
+    Add {
+        #[arg(short, long)]
+        owner: String,
+
+        #[arg(short, long)]
+        plate: String,
+
+        #[arg(short, long)]
+        brand: String,
+    },
+}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    if let Some(Commands::Add {
+        owner,
+        plate,
+        brand,
+    }) = cli.command
+    {
+        println!("Adding a new car with details:");
+        println!("Owner: {}", owner);
+        println!("Plate: {}", plate);
+        println!("Brand: {}", brand);
+    }
 }
