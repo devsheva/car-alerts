07990cfbd7c30b926eba97c39b137d835d340f9c
devsheva <sheshi.mateo01@gmail.com>
2025-01-04 01:32:01 +0100

    chore(core): add read_file util method

--- src/core.rs
+++ src/core.rs
@@ -1,3 +1,14 @@
+use std::{fs, io};
+
+#[cfg(not(test))]
+pub const FILE_PATH: &str = "src/cars.json";
+#[cfg(test)]
+pub const FILE_PATH: &str = "tests/cars.json";
+
 pub trait Call {
     fn call(&self);
+}
+
+pub fn read_file(path: &str) -> io::Result<String> {
+    fs::read_to_string(path)
 }
