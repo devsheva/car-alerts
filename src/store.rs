b311ea5131014064aa62cca2410509100e141948
devsheva <sheshi.mateo01@gmail.com>
2025-01-04 01:34:30 +0100

    fix(store): read file with fs instead of include_str macro

--- src/store.rs
+++ src/store.rs
@@ -1,6 +1,8 @@
-use std::{fs, path::Path};
+use std::fs;
 
 use serde::{Deserialize, Serialize};
+
+use crate::{read_file, FILE_PATH};
 
 #[derive(Debug, Serialize, Deserialize)]
 pub struct Car {
@@ -13,24 +15,43 @@ pub struct Store {}
 
 impl Store {
     pub fn load() -> Vec<Car> {
-        let serialized_cars = include_str!("cars.json");
-        serde_json::from_str(serialized_cars).expect("JSON wrong formatted")
+        let serialized_cars = read_file(FILE_PATH).expect("Failed to read file");
+        dbg!(&serialized_cars);
+        serde_json::from_str(&serialized_cars).expect("JSON wrong formatted")
     }
 
     pub fn save(cars: &[Car]) {
-        dbg!(cars);
         let json_data = serde_json::to_string(&cars).expect("Unable to serialize cars");
-        fs::write("cars.json", json_data).expect("Unable to write to file");
+        fs::write(FILE_PATH, json_data).expect("Unable to write to file");
     }
 }
 
 #[cfg(test)]
 mod tests {
+
     use super::*;
 
+    fn teardown() {
+        fs::write(FILE_PATH, "[]").expect("Unable to reset file");
+    }
+
     #[test]
-    fn test_store() {
+    fn test_load() {
         let cars = Store::load();
         assert_eq!(cars.len(), 0);
+    }
+
+    #[test]
+    fn test_save() {
+        let cars = vec![Car {
+            owner: "Mateo".to_string(),
+            plate: "1234ABC".to_string(),
+            brand: Some("Toyota".to_string()),
+        }];
+
+        Store::save(&cars);
+        assert_eq!(Store::load().len(), 1);
+
+        teardown();
     }
 }
