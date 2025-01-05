b0761cb82f2ac8069ea36183617c78fdd9289164
devsheva <sheshi.mateo01@gmail.com>
2025-01-06 00:44:04 +0100

    refactor!(core): add associated type for DTO

--- src/core.rs
+++ src/core.rs
@@ -1,12 +1,23 @@
-use std::{fs, io};
+use std::{fmt::Debug, fs, io};
+
+use clap::Args;
 
 #[cfg(not(test))]
 pub const FILE_PATH: &str = "src/cars.json";
 #[cfg(test)]
 pub const FILE_PATH: &str = "tests/cars.json";
 
-pub trait Call {
-    fn call(&self);
+pub trait DTO {
+    fn to_string(&self) -> String;
+}
+pub trait Call: Args {
+    type Output: DTO + Debug;
+
+    fn call(&self) -> Result<Self::Output, String>;
+
+    fn call_with_output(&self) {
+        println!("Not implemented")
+    }
 }
 
 pub fn read_file(path: &str) -> io::Result<String> {
