a87d9d9a6485f64b011ab17e81f639e7c3519ff2
devsheva <sheshi.mateo01@gmail.com>
2025-07-30 00:31:32 +0200

    chore: init query client

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,13 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import App from "./App";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
-const rootEl = document.getElementById('root');
-if (rootEl) {
-  const root = ReactDOM.createRoot(rootEl);
-  root.render(
-    <React.StrictMode>
+const queryClient = new QueryClient();
+
+ReactDOM.createRoot(document.getElementById("root")!).render(
+  <React.StrictMode>
+    <QueryClientProvider client={queryClient}>
+      <ReactQueryDevtools />
       <App />
-    </React.StrictMode>,
-  );
-}
+    </QueryClientProvider>
+  </React.StrictMode>,
+);
