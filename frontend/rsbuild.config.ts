22685ff3f72dc83e49ce57c0063180645716039f
devsheva <sheshi.mateo01@gmail.com>
2025-07-30 00:15:07 +0200

    chore(deps): add tailwindcss

--- frontend/rsbuild.config.ts
+++ frontend/rsbuild.config.ts
@@ -3,4 +3,11 @@ import { pluginReact } from '@rsbuild/plugin-react';
 
 export default defineConfig({
   plugins: [pluginReact()],
+  tools: {
+    postcss: {
+      postcssOptions: {
+        plugins: ['@tailwindcss/postcss'],
+      },
+    },
+  },
 });
