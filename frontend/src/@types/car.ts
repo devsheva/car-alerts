64f5c774d5bbcb33e9d186950305cdb200920ef3
devsheva <sheshi.mateo01@gmail.com>
2025-08-06 01:33:02 +0200

    chore: add list schema with api parsing

--- frontend/src/@types/car.ts
+++ frontend/src/@types/car.ts
@@ -1,7 +1,12 @@
-export type Car = {
-  owner: string
-  plate: string
-  brand: string | null
-  last_revision: Date
-  last_road_tax: Date
-}
+import z from 'zod'
+
+const Car = z.object({
+  owner: z.string(),
+  plate: z.string().optional(),
+  brand: z.string().optional(),
+  last_revision: z.string().optional(),
+  last_road_tax: z.string().optional(),
+})
+
+export type Car = z.infer<typeof Car>
+export const CarListSchema = z.array(Car)
