53fc24ff6687ac274466a3ddece36a9fc6fbd586
devsheva <sheshi.mateo01@gmail.com>
2025-08-06 13:42:01 +0200

    refactor: drop custom css in favor of tailwind classes

--- frontend/src/index.tsx
+++ frontend/src/index.tsx
@@ -1,16 +1,16 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import App from "./App";
-import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query'
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools'
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import App from './App'
 
-const queryClient = new QueryClient();
+const queryClient = new QueryClient()
 
-ReactDOM.createRoot(document.getElementById("root")!).render(
+ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <QueryClientProvider client={queryClient}>
       <ReactQueryDevtools />
       <App />
     </QueryClientProvider>
   </React.StrictMode>,
-);
+)
